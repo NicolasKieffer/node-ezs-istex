d199e197e0eeb63a2afa56b15a57a432245efd66
kieffer <nicolas.kieffer@inist.fr>
2017-03-16 14:46:18 +0100

    Add initCorpus + download

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /*jshint node:true, laxcomma:true*/
 'use strict';
 
-module.exports =  {
+module.exports = {
   ISTEXQuery: require('./lib/query.js'),
   ISTEXHarvest: require('./lib/harvest.js'),
   ISTEXRequest: require('./lib/request.js'),
   ISTEXHits: require('./lib/hits.js'),
-  ISTEXDownload : require('./lib/download.js')
+  ISTEXDownload: require('./lib/download.js'),
+  ISTEXInitCorpus: require('./lib/initCorpus.js')
 }
